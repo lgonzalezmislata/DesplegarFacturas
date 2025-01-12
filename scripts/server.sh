3b8782a55951637477d23cfb6b50e300657d1426
logongas <lorenzo.profesor@gmail.com>
2025-01-12 22:09:03 +0100

    feat(#1):Primera version

--- scripts/server.sh
+++ scripts/server.sh
@@ -7,6 +7,6 @@ sudo ufw allow $1
 echo D
 kill $(pgrep -f "java -jar Facturas-0.0.1-SNAPSHOT.jar --server.port=$1")
 echo E
-nohup ./jdk-21.0.5+11/bin/java -jar Facturas-0.0.1-SNAPSHOT.jar --server.port=$1 > app.log 2>&1 &
+nohup ./jdk-21.0.5+11/bin/java -jar Facturas-0.0.1-SNAPSHOT.jar --server.port=$1 > app-$1.log 2>&1 &
 echo F
 
