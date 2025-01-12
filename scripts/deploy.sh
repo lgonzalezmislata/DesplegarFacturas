fb8b678189812776037db899feb6dee9c6193689
logongas <lorenzo.profesor@gmail.com>
2025-01-12 20:34:51 +0100

    feat(#1):Primera version

---
+++ scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+source ./global/app.env
+source ./entornos/$1
+
+
+#Hacer el build
+cd ..
+rm -rf Facturas
+git clone $URL_SOURCE_GIT
+cd Facturas
+git switch $RAMA_GIT
+echo $(pwd)
+mvn clean install
+cd ../DesplegarFacturas
+
+#Copiar los ficheros al servidor
+
+#Reconstruir el fichero del JDK porque está en varias partes porque no deja subirlo entero
+cat ./dependencies/OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz_* > ./dependencies/OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz
+
+
+scp -i ./certificados/id_rsa ./dependencies/OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz $USER_NAME@$IP_SERVER:.
+rm ./dependencies/OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz
+scp -i ./certificados/id_rsa ./scripts/server.sh $USER_NAME@192.168.50.109:.
+scp -i ./certificados/id_rsa ../Facturas/target/Facturas-0.0.1-SNAPSHOT.jar $USER_NAME@$IP_SERVER:.
+
+#Ejecutar el script
+ssh -i ./certificados/id_rsa $USER_NAME@$IP_SERVER "./server.sh $APP_PORT"
---
+++ scripts/server.sh
@@ -0,0 +1,12 @@
+echo A
+rm -rf ./jdk-21.0.5+11
+echo B
+tar -xf OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz  -C .
+echo C
+sudo ufw allow $1
+echo D
+kill $(pgrep -f "java -jar Facturas-0.0.1-SNAPSHOT.jar --server.port=$1")
+echo E
+nohup ./jdk-21.0.5+11/bin/java -jar Facturas-0.0.1-SNAPSHOT.jar --server.port=$1 > app.log 2>&1 &
+echo F
+
